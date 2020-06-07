3a00caf2dc758bc7cf6cecd2e6c60a9d8ea3831f
Kennith Yu <yukenn@sheridancollege.ca>
2020-06-07 18:30:41 -0400

    Added id to Student Class and corresponding getter and setter.

--- src/student/Student.java
+++ src/student/Student.java
@@ -7,9 +7,18 @@ package student;
  */
 public class Student {
 
+    String  id;
     
     public static void main(String[] args) {
         
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
     
 }
